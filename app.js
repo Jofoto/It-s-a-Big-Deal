c9b612c039faf1a7069bf569452b3518e667b925
Jofoto <g-m-f@hotmail.com>
2025-03-18 20:42:46 +0200

    Handling unhandled routes

--- app.js
+++ app.js
@@ -8,17 +8,13 @@ const userRouter = require('./routes/userRouter');
 // Start express app
 const app = express();
 
-app.use(express.json());
-
 // GLOBAL MIDDLEWARES
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
-app.use((req, res, next) => {
-    console.log('Hello from the middle-earth');
-    next();
-});
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -30,6 +26,13 @@ app.use((req, res, next) => {
 //ROUTES
 app.use('/api/v1/deals', dealRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server.`
+    });
+});
 
 module.exports = app;
 
